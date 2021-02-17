6134f4ec52ab5c660e0fee1a3eb41c20ff0e26d1
AlijonXr <xurshetovalijon3@gmail.com>
2021-02-17 11:04:24 +0500

    change numbers

--- calc.c
+++ calc.c
@@ -1,26 +1,26 @@
 #include <stdio.h>
 int main(){
 
-    int son1,son2;
+    int num1,num2;
     int character;
     printf("Enter 2 numbers:\n");
-    scanf("%d%d",&son1,&son2);
+    scanf("%d%d",&num1,&num2);
     printf("1. +\t 2. -\t 3. *\t 4. /\t");
     scanf("%d",&character);
 
 
     switch(character){
     case 1:
-        printf("%d + %d = %d",son1,son2,son1 + son2);
+        printf("%d + %d = %d",num1,num2,num1 + num2);
         break;
     case 2:
-        printf("%d - %d = %d",son1,son2,son1 - son2);
+        printf("%d - %d = %d",num1,num2,num1 - num2);
         break;
     case 3:
-        printf("%d * %d = %d",son1,son2,son1 * son2);
+        printf("%d * %d = %d",num1,num2,num1 * num2);
         break;
     case 4:
-        printf("%d / %d = %d",son1,son2,son1 / son2);
+        printf("%d / %d = %d",num1,num2,num1 / num2);
         break;
     }
 return 0;
